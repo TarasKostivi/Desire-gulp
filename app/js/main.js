43a8beec8382fe5661d47b2a411ab734a2afadc1
TarasKostivi <taraskostsv@gmail.com>
2022-02-21 12:26:20 +0200

    upload all site

---
+++ app/js/main.js
@@ -0,0 +1,35 @@
+$(function () {
+  $(".header__btn").on("click", function () {
+    $(".rightside-menu").removeClass("rightside-menu--close");
+  });
+  $(".rightside-menu__close").on("click", function () {
+    $(".rightside-menu").addClass("rightside-menu--close");
+  });
+
+  $(".top__slider").slick({
+    dots: true,
+    arrows: false,
+    fade: true,
+    autoplay: true,
+  });
+
+  $(".contact-slider").slick({
+    slidesToShow: 10,
+    slidesToScroll: 10,
+    dots: true,
+    arrows: false,
+    autoplay: true
+  });
+
+  $(".artical-slider__box").slick({
+    prevArrow: '<button type="button" class="artical-slider__arrow artical-slider__arrowleft"><img src="images/arrow-left.svg" alt=""></button>',
+    nextArrow: '<button type="button" class="artical-slider__arrow artical-slider__arrowright"><img src="images/arrow-right.svg" alt=""></button>',
+    autoplay: true
+  });
+
+  var mixer = mixitup('.gallery__inner', {
+    load: {
+      filter: '.living'
+    }
+  });
+});
